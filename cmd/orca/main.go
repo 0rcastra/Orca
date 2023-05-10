e79f1ed211dfe2b7dd1b1a47677c9d80154dd007
chobobdev <chobobdev@naver.com>
2023-05-10 23:23:18 +0900

    feat: create simple http server

---
+++ cmd/orca/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "flag"
+    "log"
+    "net/http"
+    "os"
+    "strconv"
+    "time"
+
+    "github.com/gorilla/mux"
+)
+
+func main() {
+    // Parse command-line flags
+    port := flag.Int("port",8080, "the port on which to listen for incoming connections")
+    flag.Parse()
+
+    // Create the router
+    r := mux.NewRouter()
+
+    // Create the HTTP server
+    server := &http.Server{
+        Addr:           ":" + strconv.Itoa(*port),
+        Handler:        r,
+        ReadTimeout:    10 * time.Second,
+        WriteTimeout:   10 * time.Second,
+        MaxHeaderBytes: 1 << 20,
+    }
+
+    // Start the server
+    log.Printf("Starting Orca server on port %d...", *port)
+    if err := server.ListenAndServe(); err != nil {
+        log.Fatal(err)
+        os.Exit(1)
+    }
+}
+
