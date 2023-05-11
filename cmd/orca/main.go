2a6ca83deb3fef64d3e6334e8636a1d204c13114
chobobdev <chobobdev@naver.com>
2023-05-11 23:23:36 +0900

    feat: link Set Handler in main

--- cmd/orca/main.go
+++ cmd/orca/main.go
@@ -1,38 +1,41 @@
 package main
 
 import (
-    "flag"
-    "log"
-    "net/http"
-    "os"
-    "strconv"
-    "time"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 
-    "github.com/gorilla/mux"
+	"github.com/0rcastra/Orca/handler"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    // Parse command-line flags
-    port := flag.Int("port",8080, "the port on which to listen for incoming connections")
-    flag.Parse()
+	// Parse command-line flags
+	port := flag.Int("port", 8080, "the port on which to listen for incoming connections")
+	flag.Parse()
 
-    // Create the router
-    r := mux.NewRouter()
+	// Create the router
+	r := mux.NewRouter()
 
-    // Create the HTTP server
-    server := &http.Server{
-        Addr:           ":" + strconv.Itoa(*port),
-        Handler:        r,
-        ReadTimeout:    10 * time.Second,
-        WriteTimeout:   10 * time.Second,
-        MaxHeaderBytes: 1 << 20,
-    }
+	// Defining the commands
+	r.HandleFunc("/set/{key}/{value}", handler.SetHandler).Methods("POST")
 
-    // Start the server
-    log.Printf("Starting Orca server on port %d...", *port)
-    if err := server.ListenAndServe(); err != nil {
-        log.Fatal(err)
-        os.Exit(1)
-    }
+	// Create the HTTP server
+	server := &http.Server{
+		Addr:           ":" + strconv.Itoa(*port),
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	// Start the server
+	log.Printf("Starting Orca server on port %d...", *port)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 }
-
