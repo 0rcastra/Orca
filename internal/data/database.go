125a16927a3819b8b7e923a5281e34fa296b9291
chobobdev <chobobdev@naver.com>
2023-05-14 16:04:26 +0900

    feat: change `string` to `interface` for values

--- internal/data/database.go
+++ internal/data/database.go
@@ -6,13 +6,13 @@ import (
 
 // Database Struct
 type Database struct {
-	data  map[string]string
+	data  map[string]interface{}
 	mutex sync.RWMutex
 }
 
 // creates a new instance of the Database.
 func NewDatabase() *Database {
 	return &Database{
-		data: make(map[string]string),
+		data: make(map[string]interface{}),
 	}
 }
