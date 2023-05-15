db54ef8509a60cf515c07514d984d3dbeb54c4f2
chobobdev <chobobdev@naver.com>
2023-05-16 00:46:27 +0900

    test : fix misleading test case

--- test/data/set_test.go
+++ test/data/set_test.go
@@ -11,16 +11,16 @@ func TestSet(t *testing.T) {
 
 	db.Set("name", "seongbin")
 	if value, exists := db.Get("name"); !exists || value != "seongbin" {
-		t.Errorf("Set failed: expected value 'value1', got '%s'", value)
+		t.Errorf("Set failed: expected value 'seongbin', got '%s'", value)
 	}
 	db.Set("age", "23")
 
 	if value, exists := db.Get("age"); !exists || value != "23" {
-		t.Errorf("Set failed: expected value 'value2', got '%s'", value)
+		t.Errorf("Set failed: expected value '23', got '%s'", value)
 	}
 
 	db.Set("", "chobobdev")
-	if value, exists := db.Get(""); exists || value != "" {
-		t.Errorf("Set failed: expected empty value for empty key")
+	if value, exists := db.Get(""); !exists || value != "chobobdev" {
+		t.Errorf("Set failed: expected value 'chobobdev', got '%s'", value)
 	}
 }
