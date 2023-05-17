2391d837bc70f1e42ab3b4d014971d66afb3b3af
chobobdev <chobobdev@naver.com>
2023-05-17 19:21:40 +0900

    test : test code for get.go

---
+++ cli/command/get_test.go
@@ -0,0 +1,52 @@
+package command_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/0rcastra/Orca/cli/command"
+	"github.com/0rcastra/Orca/internal/data"
+	"github.com/0rcastra/Orca/utils"
+)
+
+func TestGetCommand_Execute(t *testing.T) {
+	db := data.NewDatabase()
+	setCmd := &command.SetCommand{Database: db}
+	setErr := setCmd.Execute([]string{"name", "Seongbin"})
+	if setErr != nil {
+		t.Fatalf("unexpected error setting value: %s", setErr.Error())
+	}
+
+	cmd := &command.GetCommand{Database: db}
+
+	expectedOutput := "Seongbin"
+	output, err := utils.CaptureOutput(func() {
+		cmdErr := cmd.Execute([]string{"name"})
+		if cmdErr != nil {
+			t.Errorf("unexpected error: %s", cmdErr.Error())
+		}
+	})
+	if err != nil {
+		t.Fatalf("failed to capture output: %s", err.Error())
+	}
+	if strings.TrimSpace(output) != expectedOutput {
+		t.Errorf("unexpected output: got %q, want %q", strings.TrimSpace(output), expectedOutput)
+	}
+
+	expectedErrorMessage := "key nonexistent not found"
+	output, err = utils.CaptureOutput(func() {
+		cmdErr := cmd.Execute([]string{"nonexistent"})
+		if cmdErr == nil {
+			t.Errorf("expected error, got nil")
+		}
+		if !strings.Contains(cmdErr.Error(), expectedErrorMessage) {
+			t.Errorf("unexpected error message: got %q, want %q", cmdErr.Error(), expectedErrorMessage)
+		}
+	})
+	if err != nil {
+		t.Fatalf("failed to capture output: %s", err.Error())
+	}
+	if output != "" {
+		t.Errorf("unexpected output: got %q, want empty string", output)
+	}
+}
