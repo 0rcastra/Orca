b46308f9b115126896fe1331ab8e03f650821b53
chobobdev <chobobdev@naver.com>
2023-05-21 00:23:03 +0900

    test : improve line coverage

--- handler/del_test.go
+++ handler/del_test.go
@@ -15,32 +15,53 @@ func TestDelHandler(t *testing.T) {
 	db := data.NewDatabase()
 	db.Set("name", "Seongbin")
 
-	req := httptest.NewRequest(http.MethodDelete, "/del/name", nil)
+	req1 := httptest.NewRequest(http.MethodDelete, "/del/name", nil)
+	res1 := httptest.NewRecorder()
+	h1 := handler.NewHandler(db)
+	router1 := mux.NewRouter()
+	router1.HandleFunc("/del/{key}", h1.DelHandler).Methods(http.MethodDelete)
+	router1.ServeHTTP(res1, req1)
 
-	res := httptest.NewRecorder()
-
-	h := handler.NewHandler(db)
-
-	router := mux.NewRouter()
-	router.HandleFunc("/del/{key}", h.DelHandler).Methods(http.MethodDelete)
-
-	router.ServeHTTP(res, req)
-
-	if res.Code != http.StatusOK {
-		t.Errorf("unexpected status code: got %d, want %d", res.Code, http.StatusOK)
+	if res1.Code != http.StatusOK {
+		t.Errorf("unexpected status code: got %d, want %d", res1.Code, http.StatusOK)
 	}
 
-	var response handler.DelResponse
-	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+	var response1 handler.DelResponse
+	if err := json.NewDecoder(res1.Body).Decode(&response1); err != nil {
 		t.Errorf("failed to decode response body: %s", err.Error())
 	}
 
-	expectedKey := "name"
-	expectedDeleted := true
-	if response.Key != expectedKey {
-		t.Errorf("unexpected response key: got %s, want %s", response.Key, expectedKey)
+	expectedKey1 := "name"
+	expectedDeleted1 := true
+	if response1.Key != expectedKey1 {
+		t.Errorf("unexpected response key: got %s, want %s", response1.Key, expectedKey1)
 	}
-	if response.Deleted != expectedDeleted {
-		t.Errorf("unexpected response deleted value: got %v, want %v", response.Deleted, expectedDeleted)
+	if response1.Deleted != expectedDeleted1 {
+		t.Errorf("unexpected response deleted value: got %v, want %v", response1.Deleted, expectedDeleted1)
+	}
+
+	req2 := httptest.NewRequest(http.MethodDelete, "/del/nonexistent", nil)
+	res2 := httptest.NewRecorder()
+	h2 := handler.NewHandler(db)
+	router2 := mux.NewRouter()
+	router2.HandleFunc("/del/{key}", h2.DelHandler).Methods(http.MethodDelete)
+	router2.ServeHTTP(res2, req2)
+
+	if res2.Code != http.StatusNotFound {
+		t.Errorf("unexpected status code: got %d, want %d", res2.Code, http.StatusNotFound)
+	}
+
+	var response2 handler.DelResponse
+	if err := json.NewDecoder(res2.Body).Decode(&response2); err != nil {
+		t.Errorf("failed to decode response body: %s", err.Error())
+	}
+
+	expectedKey2 := "nonexistent"
+	expectedDeleted2 := false
+	if response2.Key != expectedKey2 {
+		t.Errorf("unexpected response key: got %s, want %s", response2.Key, expectedKey2)
+	}
+	if response2.Deleted != expectedDeleted2 {
+		t.Errorf("unexpected response deleted value: got %v, want %v", response2.Deleted, expectedDeleted2)
 	}
 }
