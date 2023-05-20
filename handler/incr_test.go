ec922021139682e100279e5b1a6b53b5b03e5bd9
chobobdev <chobobdev@naver.com>
2023-05-21 01:33:19 +0900

    test : test code for handler/incr.go

---
+++ handler/incr_test.go
@@ -0,0 +1,47 @@
+package handler_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/0rcastra/Orca/handler"
+	"github.com/0rcastra/Orca/internal/data"
+	"github.com/gorilla/mux"
+)
+
+func TestIncrHandler(t *testing.T) {
+	db := data.NewDatabase()
+	db.Set("count", "5")
+
+	req := httptest.NewRequest(http.MethodPut, "/incr/count", nil)
+
+	res := httptest.NewRecorder()
+
+	h := handler.NewHandler(db)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/incr/{key}", h.IncrHandler).Methods(http.MethodPut)
+
+	router.ServeHTTP(res, req)
+
+	if res.Code != http.StatusOK {
+		t.Errorf("unexpected status code: got %d, want %d", res.Code, http.StatusOK)
+	}
+
+	var response handler.IncrResponse
+	err := json.NewDecoder(res.Body).Decode(&response)
+	if err != nil {
+		t.Errorf("failed to decode response body: %s", err.Error())
+	}
+
+	expectedKey := "count"
+	expectedValue := 6
+	if response.Key != expectedKey {
+		t.Errorf("unexpected response key: got %s, want %s", response.Key, expectedKey)
+	}
+	if response.Value != expectedValue {
+		t.Errorf("unexpected response value: got %d, want %d", response.Value, expectedValue)
+	}
+}
